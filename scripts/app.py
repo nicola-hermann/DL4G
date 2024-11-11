b6e759bbed6b9dc4b0c8572f5d4d0c5fc0ff9de2
Nicola Hermann <nicola.hermann@bluewin.ch>
2024-11-11 16:05:50 +0100

    Prep work

--- scripts/app.py
+++ scripts/app.py
@@ -2,6 +2,9 @@ import logging
 
 from jass.service.player_service_app import PlayerServiceApp
 from dl4g.agents.baseline_agent import BaselineAgent
+from dotenv import load_dotenv
+import os
+from flask_cors import CORS
 
 
 def create_app():
@@ -14,9 +17,12 @@ def create_app():
         flask run --host=0.0.0.0 --port=8888
     """
     logging.basicConfig(level=logging.DEBUG)
+    load_dotenv()
 
     # create and configure the app
     app = PlayerServiceApp("player_service")
+
+    app.config["DEBUG"] = os.environ.get("FLASK_DEBUG", False)
 
     # you could use a configuration file to load additional variables
     # app.config.from_pyfile('my_player_service.cfg', silent=False)
@@ -24,7 +30,7 @@ def create_app():
     # add some players
     app.add_player("random", BaselineAgent())
 
-    return app
+    return CORS(app)
 
 
 if __name__ == "__main__":
